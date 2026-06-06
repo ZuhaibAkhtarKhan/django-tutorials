ab04dc8b3194ea27665566989ec5cbb5b93ae251
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-06-06 15:31:45 +0500

    Create models and choices

--- Mosh-1hr-YT/storefront/urls.py
+++ Mosh-1hr-YT/storefront/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for storeproject project.
+URL configuration for storefront project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
