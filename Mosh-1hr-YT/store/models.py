5c0c51dafd5831f28c32d7ab3746245f38bbc2be
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-06-06 17:27:50 +0500

    Types of relationships between model classes

--- Mosh-1hr-YT/store/models.py
+++ Mosh-1hr-YT/store/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+
+#many to many
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    # product_set
+
+
 class Product(models.Model):
     # sku = models.CharField(max_length=10, primary_key=True)
     title = models.CharField(max_length=255) #varchar(255)
@@ -8,6 +19,11 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    # 1 to many
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+    #many to many
+    promotions = models.ManyToManyField(Promotion) #, related_name='products') #related_name is the name to be used in the promotion class instead of default
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -27,6 +43,8 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+
+
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
@@ -40,3 +58,41 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    # 1 to many
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+# 1 to 1 relation
+
+# class Address(models.Model):
+#     street = models.CharField(max_length=255)
+#     city = models.CharField(max_length=255)
+#     customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+# 1 to many relations
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+# many to many
+
+
+
+
+
