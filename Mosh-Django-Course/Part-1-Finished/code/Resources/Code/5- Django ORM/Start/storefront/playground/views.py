5453037c86413327de998819fcbd40aaa8b31241
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-06-25 00:54:23 +0500

    restructure

---
+++ Mosh-Django-Course/Part-1-Finished/code/Resources/Code/5- Django ORM/Start/storefront/playground/views.py
@@ -0,0 +1,234 @@
+from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
+from store.models import Product, Customer, Collection, Order, OrderItem
+from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
+from django.db.models.aggregates import Count, Max, Min, Avg, Sum
+from django.db.models.functions import Concat
+from django.db import transaction, connection
+
+# querying generic relations
+from django.contrib.contenttypes.models import ContentType
+from tags.models import TaggedItem
+# Product already imported
+
+
+# @transaction.atomic() # DEcorator # Anything in the wrap will be run when I run the say_hello function
+def say_hello(request):
+
+    # try:
+
+    #     # product = null, no exception at all
+    #     exists = Product.objects.filter(pk = 0).exists() # We get a query, that is evaluated later
+    # # count() function do not return a query set, it returns a number (count), and there are other functions(eg get()) as well, that do nmot return a query set
+    # except ObjectDoesNotExist:
+    #     pass
+
+    # queryset api (field lookups) cuz unit_price > 20 returns boolean which filter do not expect 
+    # queryset = Product.objects.filter(last_update__year = 2021)
+
+    # q1 = Customer.objects.filter(email__icontains='.com')
+
+    # q2 = Collection.objects.filter(featured_product__isnull = True)
+
+    # q3 = Product.objects.filter(inventory__lt = 10)
+
+    # q4 = Order.objects.filter(customer__id = 1)
+
+    # q5 = OrderItem.objects.filter(product__collection__id=3)
+
+    # Product: inventory < 10 AND price < 20
+
+    # queryset = Product.objects.filter(inventory__lt = 10, unit_price__lt = 20)
+    # queryset = Product.objects.filter(inventory__lt = 10).filter(unit_price__lt = 20)
+
+    # Product: inventory < 10 OR price < 20 (Q objects)
+    # queryset = Product.objects.filter(Q(inventory__lt = 10) | ~Q(unit_price__lt = 20))
+
+    # comparing two fields (F Objects)
+    # queryset = Product.objects.filter(inventory = F('collection__id'))
+
+    #Sorting
+
+    # queryset = Product.objects.order_by('unit_price','-title').reverse()
+
+    # product = Product.objects.order_by('unit_price')[0]
+    # product = Product.objects.earliest('unit_price') # sort the result by unit price in ASC and get the first object
+    # product = Product.objects.latest('unit_price') # sort the result by unit price in DSC and get the first object
+
+
+    # LIMITING RESULTS
+    # queryset = Product.objects.all()[5:10]
+
+    # SELECTING FIELDS TO QUERY
+    # queryset = Product.objects.values_list('id', 'title', 'collection__title') # values gives dicts and values_list gives tuples
+
+    
+    # queryset = Product.objects.filter(id__in =OrderItem.objects.values('product_id').distinct()).order_by('title')
+
+    # queryset = OrderItem.objects.values('product__title').distinct().order_by('product__title') # Just had to chnage the html
+
+
+    # DERERRING FIELDS
+    # queryset = Product.objects.only('id', 'title')
+    # only returns the object instsances while values return dictionaries
+    # only make objects, so if a non-existing parameter is typed in it, for each object, it will run a sql query trying to fetch that non-existing parameter
+
+    # defer is the opposite of only
+    # queryset = Product.objects.defer('description')
+
+    # SELECTING RELATED OBJECTS
+    # queryset = Product.objects.select_related('collection').all()
+    # queryset = Product.objects.select_related('collection__someOtherField').all()
+    # select_related is kinda join in sql, so that there wouldn't be a second derived query and works for 1 to many (not many to one (Reverse foreign key))
+
+    # for many to one, many to many and reverse foreign key, we got prefetch_related
+    # queryset = Product.objects.prefetch_related('promotions').all()
+
+    # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
+
+    # queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+
+
+    # aggregate
+
+    # result = Product.objects.filter(collection__id=1).aggregate(count = Count('id'), min_price = Min('unit_price'))
+
+    # result = Order.objects.aggregate(count = Count('id'))
+
+    # result = OrderItem.objects.filter(product__id = 1).aggregate(count = Sum('quantity'))
+
+    # result = Order.objects.filter(customer__id = 1).aggregate(count = Count('id'))
+
+
+    # queryset = Customer.objects.annotate(is_new=Value(True))
+    # if there isnt Value() then the django willl search for Column True, which it wouldnt find to rename throwing an error. Now as it is as Value, True is filled in every column named is_new
+
+    # queryset = Customer.objects.annotate(is_new=F('id'))
+
+    # we can perform computations as well.
+
+    # queryset = Customer.objects.annotate(is_new=F('id')+1)
+
+
+    # queryset = Customer.objects.annotate(
+    #     # CONCAT
+    #     full_name = Func(F('first_name'),Value(' '), F('last_name'), function='CONCAT')
+    # )
+
+    # another method is to use concat function from django, we have to import it first
+
+
+    # queryset = Customer.objects.annotate(
+    #     # CONCAT USING DjNAGO FUNCTION
+    #     full_name = Concat('first_name',Value(' '), 'last_name')
+    #     # BENEFIT IS THAT WE DO NOT hAVE TO RAP IT IN AN F OBJECT BUt FOR A WHITE SPACE WE HAVE T WRAP IT INSIDE A VALUE OBJECT
+    # )
+
+    #GROUPING
+
+    # queryset = Customer.objects.annotate(
+    #     orders_count = Count('order')
+    # )
+
+    # EXPRESSION WRAPPER
+
+    # queryset = Product.objects.annotate(
+    #     discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
+    # )
+
+
+    # Content Types (Generic relations)
+
+    # Get for model gives the content_object_id for the class in it 
+    # content_type = ContentType.objects.get_for_model(Product)
+
+    # queryset = TaggedItem.objects.select_related('tag').filter(
+    #     content_type = content_type,
+    #     object_id = 1
+    # )
+
+    # Custom methods
+    # Go to models.py where you wanna create
+    # created get_tag_for there
+
+    # queryset = TaggedItem.objects.get_tags_for(Product, 1)
+
+
+    # Understanding QuerySet Cache
+
+    # CREATING OBJECTS
+
+    # collection = Collection() # WE can also do it inside teh brackets like Collection(collecion.title = 'Video Games') -> not recommended
+    # collection.title = 'Video Games'
+    # # collection.featured_product = Product(pk=1) # OR
+    # collection.featured_product_id = 1
+    # collection.save() # save the data
+    # # if id isn't typed, then it will make a new id and if id is types
+
+    # we can also like this
+    # collection = Collection.objects.create(title='a', featured_product_id = 1)
+
+
+    # UPDATING OBJECTS
+    # collection = Collection(pk=11)
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
+
+    # WE must mention all the fields set to something or we will get data loss.
+
+    # # we can also do
+    # collection = Collection.objects.get(pk=11)
+    # collection.featured_product = None
+    # collection.save()
+
+    # # also updating directly in teh database
+    # Collection.objects.filter(id = 11).update(featured_product= None) # filter is necessary else all the colums featured_product will be updated to None
+
+
+    # DELETING OBJECTS
+    # collection = Collection(pk=11)
+    # collection.delete()
+
+    # Collection.objects.filter(id__gt=10).delete()
+
+
+
+    # TRANSACTIONS
+
+    # with transaction.atomic():
+    #     order = Order()
+    #     order.customer_id = 1
+    #     order.save()
+
+    #     item = OrderItem()
+    #     item.order = order
+    #     item.product_id = 1
+    #     item.quantity = 1
+    #     item.unit_price = 10
+
+    #     item.save()
+
+
+    # Executing RAW SQL QUERIES
+    # queryset = Product.objects.raw('SELECT * FROM store_product') # In the select statement, the PK mus tbe included, else an error.
+    # # WE CANT DO FILTER METHOD ETC ON THIS queryset
+    #instead import connection on top and;
+    # cursor = connection.cursor()
+    # cursor.execute('')
+    # cursor.close()
+
+    # # we can do thi sas well so not to worry about closing
+    # with connection.cursor() as cursor:
+    #     cursor.execute('')
+
+    #     # cursor.callproc('get_customers', [1, 2, 'a']) # to call stored procedures
+
+    # cursor = connection.cursor()
+    # cursor.execute('SELECT * FROM store_product')
+    # queryset = cursor.fetchall()
+    # cursor.close()
+
+    
+    return render(request, 'hello.html', {'name': 'Zuhaib'})
---
+++ Mosh-Django-Course/Part-1-Finished/code/Resources/Code/5- Django ORM/Start/storefront/store/admin.py
@@ -0,0 +1,214 @@
+from django.contrib import admin, messages
+from . import models
+from django.utils.html import format_html, urlencode
+from django.db.models import Count
+from django.urls import reverse
+# from tags.models import TaggedItem # now store_custom manages it
+
+# from django.contrib.contenttypes.admin import GenericTabularInline
+
+
+# class InventoryFilter(admin.SimpleListFilter):
+#     title = 'Inventory'
+#     parameter_name = 'inventory' 
+
+#     def lookups(self, request, model_admin):
+#         return [
+#             ('<10', 'low')
+#         ]
+
+#     def queryset(self, request, queryset):
+#         if self.value() == '<10':
+#             return queryset.filter(inventory__lt=10)
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'Inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'low'),
+            ('>10', 'Ok')
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+        else:
+            return queryset.filter(inventory__gt=10)
+
+
+# move this class to store_custom admin.py so that both the tags and store apps remain independent
+
+# class TagInline(GenericTabularInline):
+#     model = TaggedItem
+#     autocomplete_fields = ['tag']
+
+# customizing the list page
+# how we wanna view our Product in admin
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    # showing limited options for a field
+    autocomplete_fields = ['collection']
+    # inlines = [TagInline] # Is now part of the Custom..... in store_custom admin.py
+    #when we wanna show certain fields for adding a product
+    # fields=['title', 'slug']
+    # exclude=['unit_price'] # opposite of fields
+    # readonly_fields = ['title']
+
+    # PREPOPULATING SLUG FIELD
+    prepopulated_fields = {
+        'slug': ['title']
+    }
+
+
+    # collection included to show related class Collection
+    # as collection __str__ function is set to title, so we will be getting titles instead of Objext addresses, to get id etc still, deifne that method
+    list_display  = ['title', 'unit_price', 'inventory_status', 'collection_title']
+    # make editable on list page
+    list_editable = ['unit_price']
+    list_per_page = 10
+    list_select_related = ['collection'] # To avoid extra queries
+    #must be added
+    actions=['clear_inventory']
+
+    # adding filters # adding custom filter (making InventoryFilter class)
+    list_filter=['collection', 'last_update', InventoryFilter]
+    search_fields=['title']
+
+    # Adding computed fields/columns
+    @admin.display(ordering='inventory') # used to set meta data properties on the function as if the user clicks this column header, sort the data using the inventory field
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'low'
+        return 'Ok'
+    
+    # method to get collection title or any other field
+    def collection_title(self, product):
+        return product.collection.title
+    
+
+    # Making custom actions 
+    # @admin.action(description='Clear Inventory')
+    # def clear_inventory(self, request, queryset):
+    #     updated_count = queryset.update(inventory=0)
+    #     self.message_user(
+    #         request,
+    #         f'{updated_count} inventories set to 0',
+    #         messages.SUCCESS
+    #     )
+
+    @admin.action(description='Clear inventory')
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request,
+            f'{updated_count} records updated successfully',
+            messages.SUCCESS
+        )
+
+class OrderItemInline(admin.TabularInline): # Also try stackedinline for stacked disaply
+    autocomplete_fields=['product']
+    model = models.OrderItem
+    extra=0 # not to see extra rows
+    min_num = 1
+    max_num = 10
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    list_per_page = 10
+    list_select_related = ['customer']
+
+    autocomplete_fields=['customer']
+    inlines = [OrderItemInline]
+
+
+    # def customer_name(self, order):
+    #     return order.customer.first_name + order.customer.last_name
+
+
+# or this and admin.site.register(models.Product, ProductAdmin) wcommented  in next lines
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display  = ['title', 'unit_price']
+
+
+# @admin.register(models.Customer)
+# class CustomerAdmin(admin.ModelAdmin):
+#     list_display = ['first_name', 'last_name', 'membership', 'order_count']
+#     list_editable = ['membership']
+#     list_per_page = 10
+#     ordering = ['first_name', 'last_name'] # we can do ordering here as well instead of meta
+
+
+#     @admin.display(ordering='order_count')
+#     def order_count(self, customer):
+#         url = (reverse('admin:store_order_changelist') + '?' + urlencode({
+#             'customer__id':str(customer.id)
+#         }))
+#         return format_html('<a href="{}">{}</a>', url, customer.order_count)
+    
+#     def get_queryset(self, request):
+#         return super().get_queryset(request).annotate(
+#             order_count = Count('order')
+#         )
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership', 'order_count']
+    list_editable = ['membership']
+    list_per_page = 10
+    ordering=['first_name', 'last_name']
+    search_fields=['first_name__istartswith', 'last_name__istartswith']
+
+    @admin.display(ordering='order_count')
+    def order_count(self, customer):
+
+        # return customer.order_count
+        url = (reverse('admin:store_order_changelist') + '?' + urlencode({
+            'customer__id': str(customer.id)
+        }))
+        return format_html('<a href="{}" >{}</a>', url, customer.order_count )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            order_count = Count('order')
+        )
+    
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'product_count']
+
+    # to be searhced through title in the pRoduct list page while drop down
+    search_fields= ['title']
+
+    @admin.display(ordering='product_count')
+    def product_count(self, collection):
+        # return collection.product_count # WHICH DOESNT EXIST SO definging get_queryset(builtin fucntion but we have to customize)
+        # reverse('admin:app_model_page')
+        url = (reverse('admin:store_product_changelist') + '?' + urlencode({
+            'collection__id': str(collection.id)
+        }))
+        return format_html('<a href="{}"> {} </a>',url, collection.product_count)
+        # return collection.product_count
+        # now we will add a html link whenever we tap on the product_count to jump to the products
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            product_count = Count('product')
+        )
+    # the default query to be returned is added with the annotate method
+
+
+
+
+
+
+# Register your models here.
+
+# admin.site.register(models.Collection)
+# admin.site.register(models.Product, ProductAdmin)
