72fa39777db56357ba6d290dea63f96cec5b86e6
zuhaibakhtarkhan <zuhaibakhtarkhan@gmail.com>
2026-06-29 23:50:21 +0500

    Cart API

--- Mosh-Django-Course/Part-2/storefront/store/models.py
+++ Mosh-Django-Course/Part-2/storefront/store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -97,11 +98,13 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
+    # universal unique identifier
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
 
